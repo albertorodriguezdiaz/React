2d2225ec5e5f7a3a71f561b9ca8a50bd7afc15d8
albertorodriguezdiaz <alberto@efectovirtual.com>
2019-12-28 22:43:41 -0500

    Proyecto Citas Pacientes

--- Ejercicios/firstreact/src/App.js
+++ Ejercicios/firstreact/src/App.js
@@ -1,22 +1,11 @@
 import React, {Fragment} from 'react';
+import PrimerComponente from './components/PrimerComponente';
+
 
 function App() {
 
-  const empleado ={
-    nombre: 'Alberto Rodriguez',
-    trabajo: 'Desarrollador Web'
-  }
   return (
-    // React.createElement(
-    //   'h1',
-    //   {id: 'heading', className: 'heading'},
-    //   'Bievenidos'
-    // )
-
-    <Fragment>
-    <h1>{empleado.nombre}</h1>
-    <p>{empleado.trabajo}</p>
-    </Fragment>
+    <PrimerComponente />
   );
 }
 
