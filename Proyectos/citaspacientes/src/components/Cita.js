bffe8f721c9901843c18cf4115aae2f16c911cb2
albertorodriguezdiaz <alberto@efectovirtual.com>
2020-01-01 23:18:52 -0500

    Listas Citas

---
+++ Proyectos/citaspacientes/src/components/Cita.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Cita = ({cita}) => (
+    <div className="media mt-3">
+        <div className="media-body">
+            <h3 className="mt-0">
+                {cita.paciente}
+            </h3>
+            <p className="card-text"><span>Paciente: </span> {cita.paciente} </p>
+            <p className="card-text"><span>Acompañante: </span> {cita.aconpanante} </p>
+            <p className="card-text"><span>Fecha: </span> {cita.fecha} </p>
+            <p className="card-text"><span>Hora: </span> {cita.hora} </p>
+            <p className="card-text"><span>Sintomas: </span></p>
+            <p className="card-text"> {cita.sintomas} </p>
+        </div>
+    </div>
+);
+ 
+export default Cita;
